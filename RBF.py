eb57c500f855c58a8ab1523089de9bd9638a223f
Szymon Jabłoński <szyfzjablonski@gmail.com>
2024-05-15 13:30:53 +0200

    Add: proper plots

--- RBF.py
+++ RBF.py
@@ -2,6 +2,7 @@ import random
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.collections import PolyCollection
 
 # obliczanie aktywacji dla danego neuronu z prototypem
 def aktywacjaRBF(prototyp, dane, beta):
@@ -22,39 +23,71 @@ def runRBF(wejscie, wyniki, liczbaEpok, neuronyNaKlase, alfa, beta, wejscieKontr
     plt.ylabel("Wartość funkcji aktywacji")
     plt.show()
 
-    wykresFunkcjiAktywacji3D = [[], [] ,[], [[] for n in range(100)], [[] for n in range(100)], [[] for n in range(100)]]
-    for b in range(100):
-        for n in range(100):
-            wykresFunkcjiAktywacji3D[0].append(n / 100)
-            wykresFunkcjiAktywacji3D[1].append(b)
-            wykresFunkcjiAktywacji3D[2].append(aktywacjaRBF([n / 100], [0], b))
-            wykresFunkcjiAktywacji3D[3][b].append(n / 100)
-            wykresFunkcjiAktywacji3D[4][b].append(b)
-            wykresFunkcjiAktywacji3D[5][b].append(aktywacjaRBF([n / 100], [0], b))
+    #wykresFunkcjiAktywacji3D = [[], [] ,[], [[] for n in range(100)], [[] for n in range(100)], [[] for n in range(100)]]
+    #for b in range(100):
+    #    for n in range(100):
+    #        wykresFunkcjiAktywacji3D[0].append(n / 100)
+    #        wykresFunkcjiAktywacji3D[1].append(b)
+    #        wykresFunkcjiAktywacji3D[2].append(aktywacjaRBF([n / 100], [0], b))
+    #        wykresFunkcjiAktywacji3D[3][b].append(n / 100)
+    #        wykresFunkcjiAktywacji3D[4][b].append(b)
+    #        wykresFunkcjiAktywacji3D[5][b].append(aktywacjaRBF([n / 100], [0], b))
+#
+    #x = np.array(wykresFunkcjiAktywacji3D[0])
+    #y = np.array(wykresFunkcjiAktywacji3D[1])
+    #z = np.array(wykresFunkcjiAktywacji3D[2])
+    #x2 = np.array(wykresFunkcjiAktywacji3D[3])
+    #y2 = np.array(wykresFunkcjiAktywacji3D[4])
+    #z2 = np.array(wykresFunkcjiAktywacji3D[5])
+    ## Tworzenie wykresu 3D
+    #fig = plt.figure()
+#
+    ## syntax for 3-D projection
+    #ax = plt.axes(projection='3d')
+    #ax.view_init(15,60, 0)
+#
+    ## defining axes
+    #c = z
+    ##ax.scatter(x, y, z, c=c)
+    #ax.plot_trisurf(x, y, z, cmap='viridis_r', edgecolor='none')
+#
+    ##X, Y = np.meshgrid(x, y)
+    ##Z = X * np.exp(X)
+    ##ax.plot_surface(x2, z2, z2, cmap="plasma")
+    ## syntax for plotting
+    ##ax.set_title('3d Scatter plot geeks for geeks')
+    #plt.show()
 
-    x = np.array(wykresFunkcjiAktywacji3D[0])
-    y = np.array(wykresFunkcjiAktywacji3D[1])
-    z = np.array(wykresFunkcjiAktywacji3D[2])
-    x2 = np.array(wykresFunkcjiAktywacji3D[3])
-    y2 = np.array(wykresFunkcjiAktywacji3D[4])
-    z2 = np.array(wykresFunkcjiAktywacji3D[5])
-    # Tworzenie wykresu 3D
-    fig = plt.figure()
+    def polygon_under_graph(x, y):
+        """
+        Construct the vertex list which defines the polygon filling the space under
+        the (x, y) line graph. This assumes x is in ascending order.
+        """
+        temp = np.array([math.pow(2.71828, -y * math.pow(x[n], 2)) for n in range(len(x))])
+        return [(x[0], 0.), *zip(x, temp), (x[-1], 0.)]
+        #return [np.float_power(2.71828, -y * np.float_power(x, 2)), 0, 0]
 
-    # syntax for 3-D projection
-    ax = plt.axes(projection='3d')
-    ax.view_init(15,60, 0)
+    ax = plt.figure().add_subplot(projection='3d')
 
-    # defining axes
-    c = z
-    ax.scatter(x, y, z, c=c)
+    x = np.linspace(0., 1., 100)
+    lambdas = range(1, 10)
 
-    #X, Y = np.meshgrid(x, y)
-    #Z = X * np.exp(X)
-    #ax.plot_surface(x2, z2, z2, cmap="plasma")
-    # syntax for plotting
-    #ax.set_title('3d Scatter plot geeks for geeks')
+    # verts[i] is a list of (x, y) pairs defining polygon i.
+    gamma = np.vectorize(math.gamma)
+    verts = [polygon_under_graph(x, l)
+             for l in lambdas]
+    facecolors = plt.colormaps['viridis_r'](np.linspace(0, 1, len(verts)))
+
+    poly = PolyCollection(verts, facecolors=facecolors, alpha=.7)
+    ax.add_collection3d(poly, zs=lambdas, zdir='y')
+    ax.view_init(15, 60, 0)
+    ax.set(xlim=(0, 1), ylim=(1, 10), zlim=(0, 1),
+           xlabel='Odległość od punktu', ylabel='Wartość beta', zlabel='Wartość funkcji aktywacji')
+
     plt.show()
+
+
+
     # inne zmienne kontrolne
     celnosc = []
 
